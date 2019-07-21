3a5f1ed208d97fc17760f946407f590464c44f81
Sahan Reddy <sahan.reddy.58@gmail.com>
2019-07-21 17:21:31 -0400

    Created MotorGroup and ArcadeDrive classes Created ArcadeOpMode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArcadeOpMode.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class ArcadeOpMode extends OpMode {
+
+    DcMotor leftFront, leftBack, rightFront, rightBack;
+    MotorGroup leftMotors, rightMotors;
+
+    ArcadeDrive arcadeDrive;
+
+    @Override
+    public void init() {
+        leftFront = hardwareMap.get(DcMotor.class, "left_front");
+        leftBack = hardwareMap.get(DcMotor.class, "left_back");
+        rightFront = hardwareMap.get(DcMotor.class, "right_front");
+        rightBack = hardwareMap.get(DcMotor.class, "right_back");
+
+        leftMotors = new MotorGroup(new DcMotor[]{leftFront, leftBack});
+        rightMotors = new MotorGroup(new DcMotor[]{rightFront, rightBack});
+
+        arcadeDrive = new ArcadeDrive(leftMotors, rightMotors);
+
+    }
+
+    @Override
+    public void loop() {
+
+        arcadeDrive.drive(gamepad1.left_stick_y, gamepad1.right_stick_x);
+
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyFirstOpMode.java
@@ -0,0 +1,63 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.*;
+
+@TeleOp(name = "Ballsy OpMode", group = "Yuck")
+public class MyFirstOpMode extends OpMode {
+
+    private DcMotor leftMotor;
+    private DcMotor rightMotor;
+
+    @Override
+    public void init() {
+
+        leftMotor = hardwareMap.get(DcMotor.class, "left_motor");
+        rightMotor = hardwareMap.get(DcMotor.class, "right_motor");
+
+        leftMotor.setDirection(DcMotor.Direction.FORWARD);
+        rightMotor.setDirection(DcMotor.Direction.REVERSE);
+
+    }
+
+    @Override
+    public void loop() {
+        if (gamepad1.dpad_up) {
+            setRobotSpeed(-0.75);
+        } else if (gamepad1.dpad_down) {
+            setRobotSpeed(0.75);
+        } else if (gamepad1.dpad_left) {
+            robotTurn(0.5, 0);
+        } else if (gamepad1.dpad_right) {
+            robotTurn(0.5, 1);
+        } else {
+            stopRobot();
+        }
+    }
+
+    private void setRobotSpeed(double power) {
+        leftMotor.setPower(power);
+        rightMotor.setPower(power);
+    }
+
+    private void robotTurn(double power, int direction) {
+        switch (direction) {
+            case 0:
+                leftMotor.setPower(power);
+                rightMotor.setPower(-power);
+                break;
+            case 1:
+                leftMotor.setPower(-power);
+                rightMotor.setPower(power);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void stopRobot() {
+        setRobotSpeed(0); // wow
+    }
+
+}
